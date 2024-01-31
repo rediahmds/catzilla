07848f768a62414394ca5800c28da3282c3ee383
rediahmds_zorin <redi.ngoding@gmail.com>
2024-01-31 21:08:03 +0700

    fix: rename main function

--- catzilla.py
+++ catzilla.py
@@ -2,8 +2,8 @@ import click
 
 @click.command("catzilla")
 @click.version_option("0.0.1", prog_name="catzilla-cli")
-def hello():
+def cli():
     click.echo("Hello from Catzilla")
 
 if __name__ == "__main__":
-    hello()
+    cli()
