f8816139dfbf169e9fe7033fdf93ccdcea1286ce
Redi Ahmad <redi.ngoding@gmail.com>
2024-02-11 23:46:35 +0700

    reinitialize project with `pipenv`

--- catzilla_cli/main.py
+++
@@ -1,9 +0,0 @@
-import click
-
-@click.command("catzilla")
-@click.version_option("0.0.1", prog_name="catzilla-cli")
-def cli():
-    click.echo("Hello from Catzilla")
-
-if __name__ == "__main__":
-    cli()
