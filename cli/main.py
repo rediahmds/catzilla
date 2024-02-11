07b4621b5d125cda0276f90350c20c6f6a7e1861
Redi Ahmad <redi.ngoding@gmail.com>
2024-02-12 00:35:28 +0700

    feat: hello world

--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello, world")
+import click
 
+
+@click.command(help="Print simple hello.")
+@click.argument("name", default="world")
+def greet(name):
+    click.echo(f"Hello, {name}!")
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(greet)
 
 if __name__ == "__main__":
-    main()
+    cli()
