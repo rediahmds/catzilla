40128069477e14a4c5fed45f73d649a456620b26
Redi Ahmad <redi.ngoding@gmail.com>
2024-02-11 23:45:32 +0700

    chore: refactor app command

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='catzilla-cli',
-    version='0.0.1',
-    packages=find_packages(include=['catzilla_cli']),
+    name="catzilla-cli",
+    version="0.0.1",
+    packages=find_packages(["cli"]),
     install_requires=[
-        'Click',
+        "Click",
     ],
     entry_points={
-        'console_scripts': [
-            'catzilla = catzilla_cli.main:cli',
+        "console_scripts": [
+            "catzilla = cli.main:main",
         ],
     },
 )
