edb58a35228aaa533ffa64cbaca8d338d89e7c5f
Redi Ahmad S <90599494+rediahmds@users.noreply.github.com>
2024-02-21 16:05:09 +0700

    update with the `main` branch (#9)
    * build: add `requirements.txt`
    
    * chore: rename pylint config
    
    * chore: install `black formatter`
    
    * chrome: remove and rename pylint config
    
    * chore: black configuration
    
    * chore: configure black formatter
    
    * chore: configure `pyink`
    
    * build: replace formatter with `pyink`
    
    * refactor: format using `pyink`
    
    * Update pylint.yml
    
    * refactor: indentation 2
    
    ---------
    
    Co-authored-by: ugeleb <ugeleb67@gmail.com>
    Co-authored-by: ugeleb <159222686+ugeleb@users.noreply.github.com>

--- cli/main.py
+++ cli/main.py
@@ -3,16 +3,16 @@ import click
 
 @click.group()
 def cli():
-    pass
+  pass
 
 
 @click.command(help="Print simple hello.")
 @click.argument("name", default="world")
 def greet(name):
-    click.echo(f"Hello, {name}!")
+  click.echo(f"Hello, {name}!")
 
 
 cli.add_command(greet)
 
 if __name__ == "__main__":
-    cli()
+  cli()
